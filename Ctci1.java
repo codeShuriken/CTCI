811789654b09d64be15cbd44691fab549db3720d
Gowtham Vuppala <gowthamvuppala1@gmail.com>
2017-09-03 18:34:43 -0400

    Update and rename Solution1.java to Ctci1.java

--- Ctci1.java
+++ Ctci1.java
@@ -10,7 +10,7 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class Solution1 {
+public class Ctci1 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
